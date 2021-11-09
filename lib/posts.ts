3b798854ab45a20235a676c6ae84d59988bb4acc
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-11-09 11:41:10 -0800

    Make contentHtml optional

--- lib/posts.ts
+++ lib/posts.ts
@@ -12,7 +12,7 @@ export interface frontMatterType {
     title: string;
     date: string;
     slug: string;
-    contentHtml: string;
+    contentHtml?: string;
 }
 
 export function getSortedPostsData() {
