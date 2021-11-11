5695d414f0cb12f80ddee91ba6afe23586ef9655
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-11-10 18:50:19 -0800

    Give posts better styling

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,8 +1,14 @@
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+
+import Date from "../../components/Date";
 
 import { DefaultLayout } from "../../layouts/DefaultLayout";
 
 import { getAllPostSlugs, getPostData, frontMatterType } from "../../lib/posts";
+
+import utilStyles from "../../styles/utils.module.scss";
+import styles from "../../styles/post.module.scss";
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const postData = await getPostData(params?.slug);
@@ -24,12 +30,18 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export default function Post({ postData }: { postData: frontMatterType }) {
     return (
         <DefaultLayout>
-            {postData.title}
-            <br />
-            {postData.slug}
-            <br />
-            {postData.date}
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml! }} />
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={`${utilStyles.lightText} ${styles.date}`}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div
+                    dangerouslySetInnerHTML={{ __html: postData.contentHtml! }}
+                />
+            </article>
         </DefaultLayout>
     );
 }
