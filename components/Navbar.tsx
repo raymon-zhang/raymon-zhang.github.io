72f893a38d074af4e9130aca3301d961a34f0f22
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-11-25 19:12:26 -0800

    Add mobile hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
+import { useState } from "react";
 
 import Logo from "../icons/logo.svg";
+import MenuIcon from "../icons/menu.svg";
+import XIcon from "../icons/x.svg";
+
+import utilStyles from "../styles/utils.module.scss";
 
 const Navbar: React.FC = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     return (
         <nav className="navbar">
-            <ul>
-                <li className="logoListItem">
-                    <Link href="/">
-                        <a className="logo">
-                            <Logo /> Raymon
-                            <br />
-                            Zhang
-                        </a>
-                    </Link>
-                </li>
+            <li className="logoListItem">
+                <Link href="/">
+                    <a className="logo">
+                        <Logo /> Raymon
+                        <br />
+                        Zhang
+                    </a>
+                </Link>
+            </li>
+            <ul className={utilStyles.hiddenSmall}>
                 <li className="navLink">
                     <Link href="/about">
                         <a>about</a>
@@ -31,6 +37,35 @@ const Navbar: React.FC = () => {
                     </Link>
                 </li>
             </ul>
+            <button
+                className={`mobileMenuButton ${utilStyles.hiddenLarge}`}
+                onClick={() => {
+                    setMobileMenuOpen(!mobileMenuOpen);
+                }}
+            >
+                {mobileMenuOpen ? <XIcon /> : <MenuIcon />}
+            </button>
+            {mobileMenuOpen && (
+                <div className={`mobileMenu ${utilStyles.hiddenLarge}`}>
+                    <ul className="mobileMenuList">
+                        <li>
+                            <Link href="/about">
+                                <a>about</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">
+                                <a>contact</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/blog">
+                                <a>blog</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </nav>
     );
 };
